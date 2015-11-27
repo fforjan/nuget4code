03f00faf02bf3da4b29c0fde13d02926e999e592
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-11-27 14:43:55 -0800

    documentation update, refactor

--- src/nugetManager.ts
+++ src/nugetManager.ts
@@ -31,28 +31,74 @@ export default class NugetManager {
 					});
 	}
 
+	/**
+	 * Remove a package identified by the package id from the current project file.
+	 * 
+	 * @param packageId package to be removed
+	 */
 	public removePackage( packageId: INugetPackageId): Thenable<void> {
+		return this.getCurrentProjectFile()
+					.then(
+						(project: vscode.Uri ) =>
+							{
+								var parsedJSON: any = require(project.fsPath);
+								if (parsedJSON.dependencies.hasOwnProperty(packageId.id))
+								{
+									delete parsedJSON.dependencies[packageId.id];
+									fs.writeFileSync(project.fsPath, JSON.stringify(parsedJSON, null, 4));
+								} else {
+									throw "Packages is already not existing";
+								}
+							}
+						);
+	}
+
+	/**
+	 * Query Nuget repository for returning packages information matching the id pattern
+	 *
+	 * @param idPattern pattern to be used for lookup.
+	 */
+	public queryPackage(idPattern: string): Thenable<INugetPackageInfo[]> {
+
+		return this.getJsonResponse(this.getQueryUri(idPattern))
+				.then( (result: any) => result.data);
+	}
+
+	/**
+	 * Get the nuget query uri with a filter on a specific patern on the id
+	 * 
+	 * @param idPattern pattern to be used for filtering.
+	 */
+	private getQueryUri(idPattern: string): string {
+		return "http://api-v3search-0.nuget.org/query?q=Id:" + idPattern + "&take=10";
+	}
+
+	private getCurrentProjectFile(): Thenable<vscode.Uri> {
 		return vscode.workspace.findFiles("project.json", "")
 					.then(
 						(files: vscode.Uri[]) =>
 							{
-								var parsedJSON: any = require(files[0].fsPath);
-								if (parsedJSON.dependencies.hasOwnProperty(packageId.id))
-								{
-									delete parsedJSON.dependencies[packageId.id];
-									fs.writeFileSync(files[0].fsPath, JSON.stringify(parsedJSON, null, 4));
+								if (files.length > 1) {
+									throw "More than one project is available, cannot decide";
 								}
+
+								return files[0];
 							},
 						(reason: any) => { throw "no project.json found"; });
 	}
 
-	public queryPackage(namePattern: string): Thenable<INugetPackageInfo[]> {
-
-		return new Promise<INugetPackageInfo[]>(
-			(resolve: (value: INugetPackageInfo[] ) => void, reject: (reason?: any) => void) =>
+	/**
+	 * trigger a web request (no https supported at that time) and return the parse result.
+	 * 
+	 * @param uri URI to be used for the web request
+	 * @return the JSON parsed result of the web request
+	 */
+	private getJsonResponse(uri: string): Thenable<any> {
+		return new Promise<any>(
+			(resolve: (value: any ) => void, reject: (reason?: any) => void) =>
 				{
 					http.get(
-						this.getQueryUri(namePattern),
+						uri,
 						(resp: http.IncomingMessage) => {
 							// explicitly treat incoming data as utf8 (avoids issues with multi-byte chars)
 							resp.setEncoding("utf8");
@@ -65,16 +111,12 @@ export default class NugetManager {
 							resp.on("end", () => {
 								try {
 									var parsed: any = JSON.parse(body);
-									resolve(parsed.data);
+									resolve(parsed);
 								} catch (error) {
 									return reject(error);
 								}});
 							resp.on("error", (e: any) => { reject(e); });
 						});
 				});
-	}
-
-	private getQueryUri(namePattern: string): string {
-		return "http://api-v3search-0.nuget.org/query?q=Id:" + namePattern + "&take=10";
 	}
 }
