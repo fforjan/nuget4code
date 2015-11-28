62e2e62fe1692454a84a6df3179f21a63267f5dc
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-11-27 22:09:23 -0800

    minor changes to unit tests

--- test/nugetManager.test.ts
+++ test/nugetManager.test.ts
@@ -4,14 +4,12 @@
 //
 
 // the module 'assert' provides assertion methods from node
-import * as assert from "assert";
+import * as assert from "assert"; 
 
 import "should";
 
 // you can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from "vscode";
-
 import NugetManager from "../src/nugetManager";
 
 suite("Nuget4Code Tests", () => {
